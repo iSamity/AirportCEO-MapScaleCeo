d4bd9197195a645c6035b4650a45739f2aaad90e
iSamity <45425640+iSamity@users.noreply.github.com>
2026-01-27 23:24:00 +0100

    setup harmony patch

--- Plugin.cs
+++ Plugin.cs
@@ -1,18 +1,55 @@
-﻿using BepInEx;
+﻿using AirportCEOModLoader.WatermarkUtils;
+using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.Logging;
+using MapScaleCeo.Config;
+using MapScaleCeo.MapSize;
 
-namespace MapScaleCeo
-{
+namespace MapScaleCeo;
+
     [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
         internal static new ManualLogSource Logger;
+    internal static ConfigFile ConfigReference { get; private set; }
 
         private void Awake()
         {
             // Plugin startup logic
             Logger = base.Logger;
+        ConfigReference = base.Config;
+
+        SetupHarmony();
+
             Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
         }
+
+    private void Start()
+    {
+        SetupModLoader();
+
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_NAME} - Finished start");
+    }
+
+
+    private void SetupHarmony()
+    {
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_NAME} - Setting up Harmony.");
+
+        var harmony = new HarmonyLib.Harmony(MyPluginInfo.PLUGIN_GUID);
+        harmony.PatchAll();
+
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_NAME} - Finished up Harmony.");
+    }
+
+    private void SetupModLoader()
+    {
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_NAME} - Setting up Mod Loader.");
+
+#if DEBUG
+        WatermarkUtils.Register(new WatermarkInfo(MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION, false));
+#else
+        WatermarkUtils.Register(new WatermarkInfo("MSC", MyPluginInfo.PLUGIN_VERSION, true));
+#endif
     }
 }
